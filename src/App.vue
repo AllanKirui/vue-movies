cae7a5e1c46a20242ffc9a2f8c598914c0a1dcb1
AllanKirui <nallawilljr@gmail.com>
2021-12-23 16:24:26 +0300

    Add and register TheHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
+<template>
+  <TheHeader />
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+
+export default {
+  name: "App",
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
 <style>
 /* Import styles here */
 :root {
