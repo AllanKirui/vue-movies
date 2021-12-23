03db707ef02a12b6eb8600d0c15eeed1872db4c7
AllanKirui <nallawilljr@gmail.com>
2021-12-23 16:18:54 +0300

    Add TheHeader component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+    <button class="burger">
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+
+    <div class="logo">
+      <h1>VueMovies</h1>
+    </div>
+
+    <ul class="nav-options">
+      <li><a href="#">Movies</a></li>
+      <li><a href="#">TV Shows</a></li>
+    </ul>
+
+    <!-- Import the SearchHandler component here -->
+  </nav>
+</template>
