304ce0cde142412d6a7d82a432afe0d4a9fefddc
AllanKirui <nallawilljr@gmail.com>
2022-01-04 12:29:23 +0300

    Add styles for hamburger

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="flex flex-ai-c">
-    <button class="burger">
+    <button class="burger" @click="setActiveStatus">
       <span></span>
       <span></span>
       <span></span>
@@ -24,6 +24,55 @@ nav {
   width: 100%;
   height: 80px;
   background-color: var(--color-jet-black);
+}
+
+.burger {
+  background-color: transparent;
+  width: 24px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  z-index: 2;
+  margin-right: 30px;
+}
+
+.burger span {
+  display: block;
+  position: relative;
+  top: 0;
+  width: 24px;
+  height: 3px;
+  background-color: var(--color-clouds);
+  margin-bottom: 5px;
+  transition: 0.3s ease transform, 0.3s ease top, 0.3s ease width,
+    0.3s ease right;
+  border-radius: 2px;
+}
+
+.burger span:first-child {
+  transform-origin: 0;
+}
+
+.burger span:last-child {
+  margin-bottom: 0;
+  transform-origin: 24px;
+}
+
+.burger.open span:first-child {
+  top: -2px;
+  transform: rotateZ(45deg);
+}
+
+.burger.open span:last-child {
+  top: 6px;
+  transform: rotateZ(45deg);
+}
+
+.burger.open span:nth-child(2) {
+  width: 38px;
+  top: 2px;
+  right: 7px;
+  transform: rotateZ(-45deg);
 }
 
 .logo h1 {
