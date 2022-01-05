4d0728e0ee9b5eb56cf2446b086266d959733dd1
AllanKirui <nallawilljr@gmail.com>
2022-01-05 12:34:40 +0300

    Add html and scoped styles

---
+++ src/components/SearchHandler.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="results-wrapper">
+    <ul></ul>
+  </div>
+</template>
+
+<style scoped>
+.results-wrapper {
+  position: absolute;
+  top: 120%;
+  right: 15px;
+  width: 430px;
+  height: 250px;
+  background-color: #ececec;
+  overflow-y: scroll;
+}
+</style>
