fc5aecf4af61f9b2853bd647eb7f3cc32ee4d80c
AllanKirui <nallawilljr@gmail.com>
2022-01-06 09:18:15 +0300

    Add update to scoped styles

--- src/components/SearchHandler.vue
+++ src/components/SearchHandler.vue
@@ -8,9 +8,10 @@
 .results-wrapper {
   position: absolute;
   top: 120%;
-  right: 15px;
-  width: 430px;
+  right: 0.9375rem;
+  width: 33.125rem;
   height: 250px;
+  padding: 0.8125rem 0.625rem;
   background-color: #ececec;
   overflow-y: scroll;
 }
