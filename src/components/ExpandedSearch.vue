428f41cb838da66d898347250e5d0d3c263b21e2
AllanKirui <nallawilljr@gmail.com>
2022-01-20 16:07:23 +0300

    Add logic for showing loaded data

--- src/components/ExpandedSearch.vue
+++ src/components/ExpandedSearch.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="search-container">
     <!-- <form class="search-form" @submit.prevent="getMovies(searchLink)"> -->
-    <form class="search-form">
+    <form class="search-form" @submit.prevent="getMovies(searchLink)">
       <label for="search">Search</label>
       <input
         name="search"
         type="text"
         placeholder="Search for movies or TV shows..."
+        v-model.trim="searchTerm"
       />
       <button title="search" class="search-btn">
         <svg
@@ -54,15 +55,99 @@
   </div>
 
   <div class="results-container">
-    <h1 class="search-term">
-      Showing results for: <span class="text-white">without</span>
+    <h1 v-if="userInput" class="search-term">
+      Showing results for: <span class="text-white">{{ userInput }}</span>
     </h1>
-    <ul class="content-wrapper">
-      <li class="content"></li>
-      <li class="content"></li>
-      <li class="content"></li>
-      <li class="content"></li>
-      <li class="content"></li>
+    <ul
+      v-for="result in searchResults"
+      :key="result.id"
+      class="content-wrapper"
+    >
+      <li class="content" :title="result.title">
+        <div class="content__poster">
+          <img
+            v-if="result.poster_path"
+            :src="setPath(result.poster_path)"
+            alt=""
+          />
+          <p class="tag">Movie</p>
+        </div>
+        <div class="content__info">
+          <h3 class="content__info-title">
+            {{ setTitleLength(result.title) }}
+          </h3>
+          <div class="meta flex flex-jc-sb">
+            <p class="content__info-date">{{ result.release_date }}</p>
+            <p class="content__info-rating">
+              <img
+                src="../assets/rating-icon.svg"
+                width="15"
+                height="14.4"
+                alt="star icon"
+              />{{ result.vote_average }}
+            </p>
+          </div>
+        </div>
+      </li>
     </ul>
   </div>
 </template>
+
+<script>
+import apiKey from "../../config.js";
+const searchAPI = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query="`;
+const imgPath = "https://image.tmdb.org/t/p/w1280";
+
+export default {
+  data() {
+    return {
+      searchTerm: "",
+      userInput: "",
+      searchLink: searchAPI,
+      searchResults: [],
+    };
+  },
+  methods: {
+    setPath(poster_path) {
+      return imgPath + poster_path;
+    },
+    setTitleLength(title) {
+      if (title.length <= 20) {
+        return title;
+      }
+
+      let shortTitle = "";
+      for (let i = 0; i <= 20; i++) {
+        shortTitle += title[i];
+      }
+      return shortTitle + "...";
+    },
+    async getMovies(url) {
+      // this.isSearchActive = true;
+      // this.isLoading = true;
+      this.userInput = this.searchTerm;
+
+      // perform resets before a new fetch request
+      this.searchResults = [];
+      this.searchTerm = "";
+      // this.searchTerm = "";
+      // this.searchResults.push(this.isSearchActive);
+      // this.isMoreResults = false;
+
+      // if (this.searchTerm === "") {
+      //   this.isLoading = false;
+      //   // show no results, change routes
+      //   window.location.reload();
+      //   return;
+      // }
+
+      const response = await fetch(url + this.userInput);
+      const data = await response.json();
+      // this.isLoading = false;
+      // this.checkResults(data.results);
+      // console.log(data.results);
+      this.searchResults = data.results;
+    },
+  },
+};
+</script>
