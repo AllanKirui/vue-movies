705b71db3f400ec25457e84d9d6e605556e1ba09
AllanKirui <nallawilljr@gmail.com>
2022-02-01 23:34:42 +0300

    Add logic to fetch data

--- src/components/MovieInfo.vue
+++ src/components/MovieInfo.vue
@@ -3,7 +3,25 @@
 </template>
 
 <script>
+import apiKey from "../../config.js";
+
 export default {
   props: ["movieId"],
+  data() {
+    return {
+      result: null,
+    };
+  },
+  methods: {
+    async getMovies(id) {
+      const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`;
+
+      // fetch data
+      const response = await fetch(url);
+      const data = await response.json();
+
+      this.result = data;
+    },
+  },
 };
 </script>
