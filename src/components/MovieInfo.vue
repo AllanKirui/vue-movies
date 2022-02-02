95ed9e6ae506cd242b97df3cf6384b952e12d96a
AllanKirui <nallawilljr@gmail.com>
2022-02-02 10:06:05 +0300

    Add html & logic to show loaded data

--- src/components/MovieInfo.vue
+++ src/components/MovieInfo.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="info-wrapper"></div>
+  <div v-if="result" class="info-wrapper">
+    <div class="content-poster">
+      <img :src="setPath(result.poster_path)" alt="" width="320" />
+    </div>
+    <div class="content-text">
+      <h1 class="content-title">{{ result.title }}</h1>
+
+      <div class="meta">
+        <div class="meta-section-1">
+          <p class="content-rating">{{ result.rating }}</p>
+          <p class="content-runtime">{{ result.runtime }}</p>
+        </div>
+
+        <div class="meta-section-2">
+          <p class="content-overview">{{ result.overview }}</p>
+        </div>
+
+        <div class="meta-section-3">
+          <div class="content-country flex">
+            <p>Country</p>
+            <div
+              v-for="country in result.production_countries"
+              :key="country.name"
+            >
+              <p>{{ country.name }}</p>
+            </div>
+          </div>
+          <div class="content-genre flex">
+            <p>Genre</p>
+            <div v-for="genre in result.genres" :key="genre.id">
+              <p>
+                {{ genre.name }}
+              </p>
+            </div>
+          </div>
+          <div class="content-release flex">
+            <p>Release</p>
+            <p>{{ setDate(result.release_date) }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import apiKey from "../../config.js";
+const imgPath = "https://image.tmdb.org/t/p/w1280";
 
 export default {
-  props: ["movieId"],
+  props: ["movieId", "setDate"],
   data() {
     return {
       result: null,
     };
   },
   methods: {
+    setPath(poster_path) {
+      return imgPath + poster_path;
+    },
     async getMovies(id) {
       const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`;
 
@@ -21,6 +67,7 @@ export default {
       const data = await response.json();
 
       this.result = data;
+      console.log(this.result);
     },
   },
   watch: {
