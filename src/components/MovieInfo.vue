6b2afca7dce6e7b24ad65a722b89ef9c7000a96c
AllanKirui <nallawilljr@gmail.com>
2022-02-02 12:57:07 +0300

    Add scoped styles, update html

--- src/components/MovieInfo.vue
+++ src/components/MovieInfo.vue
@@ -1,14 +1,26 @@
 <template>
   <div v-if="result" class="info-wrapper">
     <div class="content-poster">
-      <img :src="setPath(result.poster_path)" alt="" width="320" />
+      <img
+        :src="setPath(result.poster_path)"
+        :title="result.title"
+        alt=""
+        width="320"
+      />
     </div>
     <div class="content-text">
       <h1 class="content-title">{{ result.title }}</h1>
 
       <div class="meta">
-        <div class="meta-section-1">
-          <p class="content-rating">{{ result.rating }}</p>
+        <div class="meta-section-1 flex">
+          <p class="content-rating">
+            <img
+              src="../assets/rating-icon.svg"
+              width="15"
+              height="14.4"
+              alt="star icon"
+            />{{ result.vote_average }}
+          </p>
           <p class="content-runtime">{{ result.runtime }}</p>
         </div>
 
@@ -16,27 +28,33 @@
           <p class="content-overview">{{ result.overview }}</p>
         </div>
 
-        <div class="meta-section-3">
-          <div class="content-country flex">
-            <p>Country</p>
+        <div class="meta-section-3 flex">
+          <div class="title-field flex flex-fd-c">
+            <p>Country:</p>
+            <p>Genres:</p>
+            <p>Release:</p>
+          </div>
+
+          <div class="value-field">
             <div
               v-for="country in result.production_countries"
               :key="country.name"
+              class="content-country"
             >
               <p>{{ country.name }}</p>
             </div>
-          </div>
-          <div class="content-genre flex">
-            <p>Genre</p>
-            <div v-for="genre in result.genres" :key="genre.id">
+            <div
+              v-for="genre in result.genres"
+              :key="genre.id"
+              class="content-genre"
+            >
               <p>
                 {{ genre.name }}
               </p>
             </div>
-          </div>
-          <div class="content-release flex">
-            <p>Release</p>
-            <p>{{ setDate(result.release_date) }}</p>
+            <div class="content-release flex">
+              <p>{{ setDate(result.release_date) }}</p>
+            </div>
           </div>
         </div>
       </div>
@@ -72,6 +90,7 @@ export default {
   },
   watch: {
     movieId(newValue) {
+      // call the getMovies() method to fetch movies when the movieId prop has a value
       if (newValue) {
         this.getMovies(newValue);
       }
@@ -79,3 +98,73 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.info-wrapper {
+  display: grid;
+  gap: 3.125rem;
+  grid-template-columns: 20rem auto;
+  padding: 2.1875rem 0.9375rem;
+  background-color: tomato;
+}
+
+.content-poster {
+  grid-column: 1/2;
+  width: fit-content;
+  height: fit-content;
+}
+
+.content-poster img {
+  object-fit: cover;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+    rgba(0, 0, 0, 0.22) 0px 10px 10px;
+}
+
+.content-text {
+  background-color: rgb(33, 63, 88);
+  grid-column: 2/2;
+}
+
+.content-title {
+  margin-top: 1.375rem;
+  margin-bottom: 1.125rem;
+  color: var(--color-white);
+}
+
+.meta {
+  font-size: var(--font-size-18);
+  color: var(--color-clouds);
+}
+
+.meta .meta-section-1 {
+  gap: 1.375rem;
+  margin-bottom: 1.375rem;
+}
+
+.meta .content-rating img {
+  margin-right: 0.3125rem;
+}
+
+.meta .meta-section-2 {
+  margin-bottom: 1.75rem;
+}
+
+.meta .meta-section-3 {
+  gap: 2.5rem;
+  color: var(--color-silver-chalice);
+}
+
+.meta .meta-section-3 .title-field p,
+.meta .meta-section-3 .value-field div {
+  margin-bottom: 0.3125rem;
+}
+
+.meta .meta-section-3 .content-genre p {
+  color: var(--color-white);
+}
+
+.value-field div.content-genre {
+  display: inline-flex;
+}
+</style>
