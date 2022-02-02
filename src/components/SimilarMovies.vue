981d88f1cd2955ce316894cd9abf16b46df81158
AllanKirui <nallawilljr@gmail.com>
2022-02-02 21:10:58 +0300

    Add html and logic to fetch data

--- src/components/SimilarMovies.vue
+++ src/components/SimilarMovies.vue
@@ -1,3 +1,83 @@
 <template>
-  <div class="results-container"></div>
+  <div class="results-container">
+    <!-- If there are results, show them -->
+    <div v-if="isResults">
+      <ul
+        v-for="movie in similarMovies"
+        :key="movie.id"
+        class="content-wrapper"
+      >
+        <li class="content" :title="movie.title">
+          <div class="content__poster">
+            <img
+              v-if="movie.poster_path"
+              :src="setPath(movie.poster_path)"
+              alt=""
+            />
+            <img
+              v-else
+              src="../assets/no-poster-img.svg"
+              width="70"
+              height="35.3"
+              alt=""
+              class="no-poster-img"
+            />
+            <p class="tag">Movie</p>
+          </div>
+          <div class="content__info">
+            <h3 class="content__info-title">
+              {{ setTitleLength(movie.title) }}
+            </h3>
+            <div class="meta flex flex-jc-sb">
+              <p v-if="movie.release_date" class="content__info-date">
+                {{ setDate(movie.release_date) }}
+              </p>
+              <p v-else class="content__info-date">n/a</p>
+              <p class="content__info-rating">
+                <img
+                  src="../assets/rating-icon.svg"
+                  width="15"
+                  height="14.4"
+                  alt="star icon"
+                />{{ movie.vote_average }}
+              </p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
+
+<script>
+import apiKey from "../../config.js";
+const imgPath = "https://image.tmdb.org/t/p/w1280";
+
+export default {
+  data() {
+    return {
+      similarMovies: [],
+      movieId: null,
+    };
+  },
+  methods: {
+    setPath(poster_path) {
+      return imgPath + poster_path;
+    },
+    async getMovies(id) {
+      const url = `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${apiKey}`;
+
+      // fetch data
+      const response = await fetch(url);
+      const data = await response.json();
+
+      // only get the first 8 results
+      if (data.results.length >= 8) {
+        for (let i = 0; i < 8; i++) {
+          this.similarMovies.push(data.results[i]);
+        }
+      }
+    },
+  },
+};
+</script>
