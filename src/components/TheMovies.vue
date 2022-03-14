c7685a2a2127a0fbe3f4ee42d3e773ea42669f28
AllanKirui <nallawilljr@gmail.com>
2022-03-15 00:14:43 +0300

    Add TheMovies component and route to show movies

---
+++ src/components/TheMovies.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="movies-list-wrapper flex flex-fd-c">
+    <MoviesList :page-num="activePage" />
+  </div>
+</template>
+
+<script>
+import MoviesList from "./movies/MoviesList.vue";
+
+export default {
+  name: "TheMovies",
+  components: {
+    MoviesList,
+  },
+  data() {
+    return {
+      activePage: null,
+    };
+  },
+  updated() {
+    let newPage = +this.$route.query.page;
+    this.activePage = newPage;
+  },
+};
+</script>
