4a708af98142a71202e18bc095168c83c6b783c3
AllanKirui <nallawilljr@gmail.com>
2022-03-15 00:12:45 +0300

    Add routing configuration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+
 import App from "./App.vue";
+import TheMovies from "./components/TheMovies.vue";
+import TheShows from "./components/TheShows.vue";
+import MovieInfo from "./components/movies/MovieInfo.vue";
+import ExpandedSearch from "./components/search/ExpandedSearch.vue";
+
+// create the router
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/movies", component: TheMovies },
+    { path: "/shows", component: TheShows },
+    { path: "/movies/info", component: MovieInfo },
+    { path: "/movies/search", component: ExpandedSearch },
+  ],
+});
 
 const app = createApp(App);
+
+// tell Vue to use the created router
+app.use(router);
 
 app.mount("#app");
