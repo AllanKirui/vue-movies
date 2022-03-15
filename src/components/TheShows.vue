b653dba7908d7ac88749411dd844999aa0a4e7b9
AllanKirui <nallawilljr@gmail.com>
2022-03-15 22:38:49 +0300

    Add update to logic for retrieving current page

--- src/components/TheShows.vue
+++ src/components/TheShows.vue
@@ -1,13 +1,30 @@
 <template>
-  <h2>TV Shows</h2>
+  <div class="movies-list-wrapper flex flex-fd-c">
+    <ShowsList :page-num="activePage" />
+  </div>
 </template>
 
 <script>
+import ShowsList from "./tv/ShowsList.vue";
+
 export default {
+  name: "TheMovies",
+  components: {
+    ShowsList,
+  },
   emits: ["activated-side"],
+  data() {
+    return {
+      activePage: null,
+    };
+  },
   beforeMount() {
     // emit a custom event that sets active styling on the header links
     this.$emit("activated-side", "shows");
+  },
+  updated() {
+    let newPage = +this.$route.query.page;
+    this.activePage = newPage;
   },
 };
 </script>
