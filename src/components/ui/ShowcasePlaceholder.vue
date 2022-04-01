95fc44db50d2913f66dc030be6f01bfd72f35557
AllanKirui <nallawilljr@gmail.com>
2022-04-01 18:17:02 +0300

    Add update to logic for showing carousel

--- src/components/ui/ShowcasePlaceholder.vue
+++ src/components/ui/ShowcasePlaceholder.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="showcase-wrapper">
-    <Carousel :settings="settings">
+    <Carousel :settings="carouselSettings">
       <Slide v-for="item in 8" :key="item">
         <div class="carousel__item">
-          <div class="showcase__poster animated-bg">
+          <!-- poster placeholder for large screens -->
+          <div class="showcase__poster animated-bg hide-for-small">
             <img
               src="../../assets/video-placeholder.png"
               alt="placeholder image"
               class="placeholder-img"
             />
           </div>
+          <!-- poster placeholder for small screens -->
+          <div class="showcase__poster animated-bg hide-for-large">
+            <img
+              src="../../assets/showcase-placeholder.png"
+              alt="placeholder image"
+              class="placeholder-img"
+            />
+          </div>
 
-          <!-- movie text -->
-          <div class="showcase__info">
+          <!-- movie text for large screens -->
+          <div class="showcase__info hide-for-small">
+            <h2 class="showcase__info-title animated-bg animated-bg-text"></h2>
+
+            <div class="meta">
+              <div class="meta-section-1 flex">
+                <!-- movie rating -->
+                <p
+                  class="showcase__info-rating animated-bg animated-bg-text"
+                ></p>
+                <!-- movie release date -->
+                <p class="showcase__info-date animated-bg animated-bg-text"></p>
+              </div>
+
+              <!-- movie overview -->
+              <div class="meta-section-2">
+                <p
+                  class="showcase__info-overview animated-bg animated-bg-text"
+                ></p>
+                <p
+                  class="showcase__info-overview animated-bg animated-bg-text"
+                ></p>
+                <p
+                  class="showcase__info-overview animated-bg animated-bg-text"
+                ></p>
+              </div>
+
+              <!-- more info button -->
+              <div class="view-info-link animated-bg animated-bg-text"></div>
+            </div>
+          </div>
+        </div>
+
+        <!-- movie text for small screens -->
+        <div class="mobile-info">
+          <div class="showcase__info hide-for-large">
             <h2 class="showcase__info-title animated-bg animated-bg-text"></h2>
 
             <div class="meta">
@@ -53,17 +96,41 @@ import { Carousel, Slide } from "vue3-carousel";
 import "vue3-carousel/dist/carousel.css";
 
 export default {
+  name: "ShowcasePlaceholder",
+  inject: [
+    "setSlidesBeforeScreenResize",
+    "setSlidesAfterScreenResize",
+    "setCarouselSettings",
+  ],
   components: { Carousel, Slide },
   data() {
     return {
-      settings: {
-        // carousel settings
-        wrapAround: true,
-        itemsToShow: 1.25,
-        snapAlign: "center",
-        autoplay: 8000, // 8 second duration
-      },
+      screenSize: null,
     };
+  },
+  computed: {
+    carouselSettings() {
+      // return settings before and after the screen has been resized
+      return !this.screenSize
+        ? this.setSlidesAfterScreenResize()
+        : this.setSlidesBeforeScreenResize();
+    },
+  },
+  methods: {
+    checkWindowSize() {
+      // listen to the resize event and call the method to set the info card's position
+      window.addEventListener("resize", () => {
+        this.screenSize = window.innerWidth;
+      });
+    },
+  },
+  beforeMount() {
+    // get the window's width before data is shown on the screen
+    this.screenSize = window.innerWidth;
+  },
+  mounted() {
+    // call the method after data has been loaded to the screen
+    this.checkWindowSize();
   },
 };
 </script>
@@ -90,5 +157,17 @@ export default {
 .showcase__info .meta .view-info-link {
   width: 5rem;
   cursor: unset;
+}
+
+@media screen and (min-width: 769px) {
+  .hide-for-large {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .hide-for-small {
+    display: none;
+  }
 }
 </style>
