b35be8837378bea200dfad163aab819d65fe4410
AllanKirui <nallawilljr@gmail.com>
2022-04-15 21:40:41 +0300

    Add NotFound component

---
+++ src/components/ui/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="not-found-wrapper">
+    <div class="not-found-text">
+      <h1>404</h1>
+      <h2>OOPS!... ERROR</h2>
+      <p>Looks like the page you are looking for was not found.</p>
+      <a href="#">Return to Homepage</a>
+    </div>
+    <div class="not-found-img">
+      <img src="../../assets/404-pc.svg" alt="page not found image" />
+    </div>
+  </div>
+</template>
