cdb6db77a8d813c55e6053b55a1c5e8c015712c8
AllanKirui <nallawilljr@gmail.com>
2022-04-15 22:57:24 +0300

    Add logic to show error, styling to component

--- src/components/ui/NotFound.vue
+++ src/components/ui/NotFound.vue
@@ -1,13 +1,102 @@
 <template>
-  <div class="not-found-wrapper">
+  <div class="not-found-wrapper wrapper flex flex-jc-sb flex-ai-c">
     <div class="not-found-text">
-      <h1>404</h1>
-      <h2>OOPS!... ERROR</h2>
-      <p>Looks like the page you are looking for was not found.</p>
-      <a href="#">Return to Homepage</a>
+      <h1 class="error-code">404</h1>
+      <h2 class="error-alert">OOPS!... ERROR</h2>
+      <p class="error-description">
+        Looks like the page you are looking for was not found.
+      </p>
+      <a href="#" class="home-link" title="Go Home">Return to Homepage</a>
     </div>
-    <div class="not-found-img">
-      <img src="../../assets/404-pc.svg" alt="page not found image" />
+    <div class="not-found-img-pc">
+      <img
+        src="../../assets/404-pc.svg"
+        width="380"
+        height="331"
+        alt="page not found image"
+      />
+    </div>
+    <div class="not-found-img-tree">
+      <img
+        src="../../assets/404-tree.svg"
+        width="251"
+        height="480"
+        alt="page not found image"
+      />
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "NotFound",
+  beforeMount() {
+    this.$emit("error-404");
+  },
+};
+</script>
+
+<style scoped>
+.not-found-wrapper {
+  position: relative;
+  height: 100vh;
+  padding: 0 6.25rem;
+  color: var(--color-clouds);
+}
+
+.not-found-text {
+  text-align: center;
+  z-index: 2;
+}
+
+.error-code {
+  background-image: linear-gradient(
+    to bottom,
+    var(--color-jet-black),
+    var(--color-clouds)
+  );
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -moz-background-clip: text;
+  -moz-text-fill-color: transparent;
+  font-size: 7.5rem;
+}
+
+.error-alert {
+  margin-bottom: 1rem;
+  font-weight: normal;
+  font-size: var(--font-size-28);
+}
+
+.error-description {
+  margin-bottom: 2.5rem;
+  font-size: var(--font-size-22);
+}
+
+.home-link {
+  display: inline-block;
+  padding: 8px 12px;
+  border-radius: 5px;
+  background-color: var(--color-clouds);
+  color: var(--color-smokey-black);
+  font-family: inherit;
+  font-size: var(--font-size-20);
+  cursor: pointer;
+  transition: all 0.15s ease-in-out;
+}
+
+.home-link:hover {
+  outline: var(--color-clouds) 1px solid;
+  background-color: var(--color-smokey-black);
+  color: var(--color-clouds);
+}
+
+.not-found-img-tree {
+  position: absolute;
+  top: 50%;
+  left: 10px;
+  transform: translateY(-50%);
+  z-index: 1;
+}
+</style>
